f5ed3a4125bd00a99685568f120b3482a7c24dd1
Lizandra Morais <lizandrammendonca@gmail.com>
2022-12-17 20:00:34 -0300

    Utilizando a biblioteca emoji

---
+++ Exercicio017.py
@@ -0,0 +1,13 @@
+#Desafio 17
+
+import emoji
+print("-" * 45)
+print(emoji.emojize(":trophy: - Cálculo Hipotenusa - :trophy:",variant="emoji_type"))
+print("-" * 45)
+
+cat_opo = int(input("Digite o valor do cateto oposto: "))
+cat_adj = int(input("Digite o valor do cateto adjacente: "))
+
+hipot = sqrt( pow(cat_opo,2) + pow(cat_adj,2))
+
+print("O valor da hipotenusa para o triângulo retângulo com cateto oposot {} e cateto adjacente {} é {}".format(cat_opo,cat_adj,math.trunc(hipot)))

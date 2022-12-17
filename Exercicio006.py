055ddf781729023dce596b099a77dac475f8db03
Lizandra Morais <lizandrammendonca@gmail.com>
2022-12-17 19:11:17 -0300

    Programa dobro, triplo e raiz quadrada

---
+++ Exercicio006.py
@@ -0,0 +1,6 @@
+#Desafio 6: Programa dobro, triplo e raiz quadrada
+
+n = int(input("Digite um numero: "))
+
+print(f"O dobro de {n} é {n*2} , o triplo é {n*3}" , end = " ")
+print(f"e a raiz quadrada é {n**(1/2)}")

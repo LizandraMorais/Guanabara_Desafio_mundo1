a96e814ed29cb06f26ebe4c5f6b839adc930cc38
Lizandra Morais <lizandrammendonca@gmail.com>
2022-12-17 19:48:20 -0300

    Truncando com a biblioteca math

---
+++ Exercicio016.py
@@ -0,0 +1,6 @@
+#Desafio 16: Truncando com a biblioteca math
+
+import math
+
+num = float(input("Digite um numero: "))
+print(" O {} digitado tem como parte inteira {} !".format(num,math.trunc(num)))
